19574f41b787c834fd19a689b9bf52743c1275dd
Goce Mitevski <goce.mitevski@keitaro.com>
2019-12-17 15:00:33 +0100

    Add image assets, helper functions and refactor template markup

--- ckanext/colorado/helpers.py
+++ ckanext/colorado/helpers.py
@@ -1,5 +1,6 @@
 """Colorado custom helpers.
 """
+import ckan.plugins.toolkit as toolkit
 from datetime import datetime
 from human_dates import time_ago_in_words
 
@@ -121,3 +122,30 @@ def pretty_date(str):
     t = datetime.strptime(str, '%Y-%m-%dT%H:%M:%S.%f')
 
     return time_ago_in_words(t)
+
+def get_recently_updated_datasets(limit=6):
+    '''
+     Returns recent created or updated datasets.
+    :param limit: Limit of the datasets to be returned. Default is 5.
+    :type limit: integer
+    :returns: a list of recently created or updated datasets
+    :rtype: list
+    '''
+    try:
+        pkg_search_results = toolkit.get_action('package_search')(data_dict={
+            'sort': 'metadata_modified desc',
+            'rows': limit,
+        })['results']
+
+    except toolkit.ValidationError, search.SearchError:
+        return []
+    else:
+        pkgs = []
+        for pkg in pkg_search_results:
+            package = toolkit.get_action('package_show')(
+                data_dict={'id': pkg['id']})
+            modified = datetime.strptime(
+                package['metadata_modified'].split('T')[0], '%Y-%m-%d')
+            package['days_ago_modified'] = ((datetime.now() - modified).days)
+            pkgs.append(package)
+        return pkgs
